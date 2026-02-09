d541436c83632c8d11618c8744b63b050a347a20
cheney <cheney70@qq.com>
2026-02-09 17:07:20 +0800

    swagger 配置文件格式问题

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -15,50 +15,56 @@ return [
                     'email' => 'support@example.com',
                 ],
             ],
-            'security' => [
-                'securitySchemes' => [
-                    'bearerAuth' => [
-                        'type' => 'http',
-                        'scheme' => 'bearer',
-                        'bearerFormat' => 'JWT',
-                    ],
-                ],
-                'security' => [
-                    ['bearerAuth' => []],
+            'routes' => [
+                'api' => 'api/documentation',
+            ],
+            'paths' => [
+                'annotations' => [
+                    base_path('vendor/cheney/admin-system/src/Controllers'),
+                    base_path('app/Http/Controllers'),
                 ],
             ],
-            'servers' => [
-                [
-                    'url' => env('APP_URL', 'http://localhost:8000') . '/api',
-                    'description' => 'API Server',
-                ],
-            ],
-            'paths' => [],
-            'consumes' => ['application/json'],
-            'produces' => ['application/json'],
         ],
     ],
 
     'defaults' => [
         'routes' => [
-            'api' => 'api/documentation',
+            'docs' => 'docs',
+            'oauth2_callback' => 'api/oauth2-callback',
+            'middleware' => [
+                'api' => [],
+                'asset' => [],
+                'docs' => [],
+                'oauth2_callback' => [],
+            ],
+            'group_options' => [],
         ],
         'paths' => [
-            'annotations' => [
-                base_path('vendor/cheney/admin-system/src/Controllers'),
-                base_path('app/Http/Controllers'),
-            ],
             'docs' => public_path('api-docs'),
-            'interfaces' => base_path('app/Interfaces'),
-            'traits' => base_path('app/Traits'),
-            'helpers' => base_path('app/Helpers'),
+            'views' => base_path('resources/views/vendor/l5-swagger'),
+            'base' => env('L5_SWAGGER_BASE_PATH', null),
+            'excludes' => [],
         ],
-        'scan' => [
+        'scanOptions' => [
+            'analyser' => null,
+            'analysis' => null,
+            'processors' => [],
+            'pattern' => null,
             'exclude' => [],
+            'open_api_spec_version' => env('L5_SWAGGER_OPEN_API_SPEC_VERSION', \L5Swagger\Generator::OPEN_API_DEFAULT_SPEC_VERSION),
         ],
-        'constants' => [
-            'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'http://localhost:8000'),
-            'L5_SWAGGER_CONST_SCHEMES' => env('L5_SWAGGER_CONST_SCHEMES', 'http'),
+        'securityDefinitions' => [
+            'securitySchemes' => [
+                'bearerAuth' => [
+                    'type' => 'http',
+                    'scheme' => 'bearer',
+                    'bearerFormat' => 'JWT',
+                    'description' => 'Enter token in format (Bearer <token>)',
+                ],
+            ],
+            'security' => [
+                ['bearerAuth' => []],
+            ],
         ],
         'generate_always' => env('L5_SWAGGER_GENERATE_ALWAYS', false),
         'generate_yaml_copy' => env('L5_SWAGGER_GENERATE_YAML_COPY', false),
@@ -68,31 +74,19 @@ return [
         'validator_url' => null,
         'ui' => [
             'display' => [
-                'default_models_expand_depth' => 1,
-                'default_model_expand_depth' => 1,
-                'default_modelRendering' => 'example',
-                'displayRequestDuration' => false,
-                'docExpansion' => 'none',
-                'filter' => true,
-                'maxDisplayedTags' => -1,
-                'showExtensions' => false,
-                'showCommonExtensions' => false,
-                'tryItOutEnabled' => true,
+                'dark_mode' => env('L5_SWAGGER_UI_DARK_MODE', false),
+                'doc_expansion' => env('L5_SWAGGER_UI_DOC_EXPANSION', 'none'),
+                'filter' => env('L5_SWAGGER_UI_FILTERS', true),
+            ],
+            'authorization' => [
+                'persist_authorization' => env('L5_SWAGGER_UI_PERSIST_AUTHORIZATION', false),
+                'oauth2' => [
+                    'use_pkce_with_authorization_code_grant' => false,
+                ],
             ],
         ],
-        'headers' => [
-            'Accept' => 'application/json',
+        'constants' => [
+            'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'http://localhost:8000'),
         ],
-        'middleware' => [
-            'api' => ['web'],
-        ],
-        'views' => [
-            'swagger' => 'swagger::swagger',
-            'l5-swagger' => 'swagger::index',
-        ],
-        'files' => [
-            'generator' => public_path('api-docs/swagger.json'),
-        ],
-        'algorithm' => null,
     ],
 ];
